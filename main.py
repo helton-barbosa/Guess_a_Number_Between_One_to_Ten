775448f5d57ce45ffd29bdc4e2ebc7262fa76578
Helton Barbosa <heltonbs@gmail.com>
2024-01-09 11:32:49 -0300

    Add quantidade de vidas

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import random
 
 sorteio = random.randint(1, 10)
-
+tentativas = 3
 chute = 0
 
-while chute != sorteio:
+while chute != sorteio and tentativas > 0:
+    print(f'Você tem {tentativas} tentativas [{sorteio}]')
+
     chute = int(input('Escolha um número entre [1] e [10]: '))
+    tentativas -= 1
+
     if chute < sorteio:
-        print(f'O número {chute} é menor...')
+        print(f'O número {chute} é menor...\n')
     elif chute > sorteio:
-        print(f'O número {chute} é maior...')
+        print(f'O número {chute} é maior...\n')
     else:
-        print('Você acertou!')
+        print('Você acertou!\n')
+        tentativas = 0
